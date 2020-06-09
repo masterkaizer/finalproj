e5ac79321d20592d2894367c15ef7435bc1da954
Your Name <brandon.sellam@gmail.com>
2020-06-09 20:55:11 +0200

    add a server and add stuff in  the package.json

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("dogpromenade/build"));
+}
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.send("error 404 there is an error");
+});
+
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
+
