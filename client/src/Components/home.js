53785dbd64ee7dc0545651b8ba57a762c3584acc
Your Name <brandon.sellam@gmail.com>
2020-06-10 09:00:26 +0200

    searchPage

--- client/src/Components/home.js
+++ client/src/Components/home.js
@@ -21,6 +21,8 @@ class Home extends Component {
     <button class="btn btn-outline-success" type="button"><Link to="/">Home</Link></button>
     <button class="btn btn-outline-success" type="button"><Link to="/login">Login</Link></button>
     <button class="btn btn-outline-success" type="button"><Link to="/signup">Sign up</Link></button>
+    <button class="btn btn-outline-success" type="button"><Link to="/searchPage">Search Page</Link></button>
+
  </form>
 </nav>
                 
